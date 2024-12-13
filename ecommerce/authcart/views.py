e81a4789bbb80bc1ba026593d96c3a21f1292180
Anuraj-IND <anuraj.m.11.04@gmail.com>
2024-12-13 14:54:15 +0530

    logo

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
+
+# Create your views here.
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
-    return render(request,'signup.html') 
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+        
+        if password != confirm_password:
+            return HttpResponse("password incorrect")
+            # return render(request, 'auth/signup.html')
+
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("email already exists")
+                # return render(request, 'auth/signup.html')
+        except Exception as identifier:
+            pass
+        
+        user = User.objects.create_user(email, email, password)
+        user.save()
+        return HttpResponse("User created", email)
+
+    return render(request, 'signup.html')
+
 
 def handlelogin(request):
     return render(request,'authentication/login.html')
